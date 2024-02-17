7be56f7cef7fd1d986ca6346177105fa475ed283
Mond <126099833+e-mond@users.noreply.github.com>
2024-02-17 23:21:14 +0000

    Update

--- Obj/Class/obj_class.js
+++ Obj/Class/obj_class.js
@@ -14,10 +14,51 @@ console.log(person);
 
 
  // Classes 
- class PC {
-    constructor(Monitor, SystemUnit){
-        this.Monitor = monitor;
-        this.SystemUnit = SystemUnit; 
+    class PC {
+        constructor(monitor, systemUnit){
+            this.monitor = monitor;
+            this.systemUnit = systemUnit; 
+        }
     }
+    console.log(constructor);
+
+ //Instantiating a class
+ class Profile {
+
+    constructor(id,email,passoword,address,age){
+
+    this.id = id;
+    this.email = email;
+    this.passoword = passoword;
+    this.address = address;
+    this.age = age;
+    }
+    
  }
- console.log(constructor);
+
+ const user1 = new Profile("BLACK07", "BLACK@EMAIL.COM" ,"B7@123", "AD12ST",30); 
+ console.log(user1)
+
+
+ const user2 = new Profile("ROB21", "robert@yahoo.com" ,"BOB403", "Tema",29);
+console.log(user2)
+
+
+// Creating methods in classes
+class studentDetails{
+    constructor(name, id, level, course ) {
+         this.name = name;
+         this.id = id;
+          this.level = level;
+           this.course = course;
+        }
+        getNameAndCourse(){
+            return this.name + " " + this.course;
+}
+
+}
+const freshStudent = new studentDetails ("Ablerta", "BC2202","L200","Civil Engineering");
+console.log(freshStudent.getNameAndCourse());
+
+// inheritance in classes
+ 
